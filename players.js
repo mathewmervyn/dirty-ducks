fe73b7d55159d9eb866b846de09c5843279fe87f
mathewmervyn <mathewmervyn99@gmail.com>
2026-09-04 11:28:48 -0600

    Create players.js

---
+++ players.js
@@ -0,0 +1,82 @@
+const fantasyPlayers = [
+    {
+        name: "Caleb Williams",
+        team: "CHI",
+        position: "QB"
+    },
+    {
+        name: "Bijan Robinson",
+        team: "ATL",
+        position: "RB"
+    },
+    {
+        name: "Ashton Jeanty",
+        team: "LV",
+        position: "RB"
+    },
+    {
+        name: "Trey McBride",
+        team: "ARI",
+        position: "TE"
+    }
+  {
+        name: "Zay Flowers",
+        team: "BAL",
+        position: "WR"
+    },
+    {
+        name: "Davante Adams",
+        team: "LAR",
+        position: "WR"
+    },
+    {
+        name: "Alec Pierce",
+        team: "IND",
+        position: "WR"
+    },
+    {
+        name: "Jeremiyah Love",
+        team: "ARI",
+        position: "RB"
+    }
+{
+        name: "Michael Pittman Jr.",
+        team: "PIT",
+        position: "WR"
+    },
+    {
+        name: "Tony Pollard",
+        team: "TEN",
+        position: "RB"
+    },
+    {
+        name: "Khalil Shakir",
+        team: "BUF",
+        position: "WR"
+    },
+    {
+        name: "Jacory Croskey-Merritt",
+        team: "WAS",
+        position: "RB"
+    }
+  {
+        name: "Kyler Murray",
+        team: "MIN",
+        position: "QB"
+    },
+    {
+        name: "Tyler Allgeier",
+        team: "ARI",
+        position: "RB"
+    },
+    {
+        name: "Jordyn Tyson",
+        team: "NO",
+        position: "WR"
+    },
+    {
+        name: "Tank Dell",
+        team: "HOU",
+        position: "WR"
+    }
+];
